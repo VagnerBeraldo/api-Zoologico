25e49270cd742dc0c2784d59ba48994a15f507c5
ErickGX <ericksaopaulino1@hotmail.com>
2025-12-02 23:28:47 -0300

    test: Refactor Usuario service tests and add integration tests for user creation

---
+++ src/test/java/br/com/senac/ado/zoologico/integration/UsuarioIntegrationTest.java
@@ -0,0 +1,43 @@
+package br.com.senac.ado.zoologico.integration;
+
+import br.com.senac.ado.zoologico.dto.Usuario.UsuarioDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class UsuarioIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void shouldCreateAndGetUser() throws Exception {
+
+        UsuarioDTO dto = new UsuarioDTO("user", "email@test.com", "1234");
+
+        String location = mockMvc.perform(post("/api/usuarios/registrar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getHeader("Location");
+
+        assert location != null;
+
+        mockMvc.perform(get(location))
+                .andExpect(status().isOk());
+    }
+}
